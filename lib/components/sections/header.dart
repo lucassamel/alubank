3c747d35875f4138ef5db0ee7a93358762ff2afe
Lucas Samel <Lucas.Samel@fadami.com.br>
2023-05-29 11:35:07 -0300

    Header

---
+++ lib/components/sections/header.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class Header extends StatelessWidget {
+  const Header({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: <Color>[
+            Color.fromRGBO(103, 99, 234, 1.0),
+            Color.fromRGBO(155, 105, 254, 1.0),
+            Color.fromRGBO(195, 107, 255, 1.0),
+          ],
+        ),
+        borderRadius: BorderRadius.vertical(
+          bottom: Radius.circular(10),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(16.0, 80.0, 16.0, 16.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const <Widget>[
+                // Text(
+                //   "\$1000.00",
+                //   style: TextStyle(
+                //     fontSize: 28,
+                //     fontWeight: FontWeight.bold,
+                //   ),
+                // ),
+                Text.rich(
+                  TextSpan(
+                    text: '\$',
+                    style: TextStyle(fontSize: 16),
+                    children: <TextSpan>[
+                      TextSpan(
+                        text: '1000.00',
+                        style: TextStyle(
+                            fontSize: 28, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(
+                  "Balanço disponível",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            const Icon(
+              Icons.account_circle,
+              size: 42,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
